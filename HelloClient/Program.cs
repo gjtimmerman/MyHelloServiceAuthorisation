72366f425c5718da4cf97a18cdd690bccd6e0a81
Gert Jan Timmerman <gertjan.timmerman@infosupport.com>
2021-03-01 09:01:58 +0100

    Update Program.cs

--- HelloClient/Program.cs
+++ HelloClient/Program.cs
@@ -16,7 +16,6 @@ namespace HelloClient
             ChannelFactory<MyCustomAuthService.IHello> cf = new ChannelFactory<MyCustomAuthService.IHello>("MyClientConfig");
             MyCustomAuthService.IHello proxy = cf.CreateChannel();
             Console.WriteLine(proxy.Hello(1));
-            Console.WriteLine(proxy.World(1));
             ((ICommunicationObject)proxy).Close();
         }
     }
